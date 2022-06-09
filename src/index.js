fba01323f6a7b7701bf92d98cdd6c165988ec243
mattgmdev <88032250+mattgmdev@users.noreply.github.com>
2022-06-09 16:21:01 +0200

    Updated app using React Redux

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider, connect } from "react-redux";
-import { createStore } from "redux";
+import { createStore, applyMiddleware, combineReducers } from "redux";
+import { createLogger } from "redux-logger";
+import ThunkMiddleware from "redux-thunk";
 import "./index.css";
 import App from "./containers/App";
 import registerServiceWorker from "./registerServiceWorker";
 import "tachyons";
-import { searchRobots } from "./reducers";
+import { searchRobots, requestRobots } from "./reducers";
 
-const store = createStore(searchRobots);
+const logger = createLogger();
+const rootReducer = combineReducers({ searchRobots, requestRobots });
+const store = createStore(
+  rootReducer,
+  applyMiddleware(ThunkMiddleware, logger)
+);
 
 ReactDOM.render(
   <Provider store={store}>
